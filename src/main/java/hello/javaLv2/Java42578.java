1c5dfc3a37c6d05d79373902ccbff00ea9266fd9
Hangsuu <ohhangsu@naver.com>
2024-10-30 10:50:03 +0900

    해시 의상

---
+++ src/main/java/hello/javaLv2/Java42578.java
@@ -0,0 +1,29 @@
+package hello.javaLv2;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Java42578 {
+    // 해시 > 의상
+    // courses/30/lessons/42578
+    public int solution(String[][] clothes) {
+        Map<String, Integer> clothesMap = new HashMap<>();
+
+        for(int i=0; i<clothes.length; i++) {
+            if (clothesMap.containsKey(clothes[i][1])) {
+                clothesMap.put(clothes[i][1], clothesMap.get(clothes[i][1]) + 1);
+            } else {
+                clothesMap.put(clothes[i][1], 1);
+            }
+        }
+        int answer = 1;
+
+        List<Integer> valueList = new ArrayList<>(clothesMap.values());
+        for(Integer count : valueList) {
+            answer *= count + 1;
+        }
+        return answer-1;
+    }
+}
