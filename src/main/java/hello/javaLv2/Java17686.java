949d0b2e7604e4210495f8ad5e10dde3f85d584f
Hangsuu <ohhangsu@naver.com>
2024-11-04 15:31:43 +0900

    파일명 정렬

--- src/main/java/hello/javaLv2/Java17686.java
+++ src/main/java/hello/javaLv2/Java17686.java
@@ -12,25 +12,18 @@ public class Java17686 {
     // courses/30/lessons/17686
     public String[] solution(String[] files) {
         Arrays.sort(files, (a, b) -> {
-            // 숫자 그룹을 분리하면서 보존하기 위한 정규식 패턴
-            Pattern pattern = Pattern.compile("\\d+|[^\\d]+");
-            Matcher matcherA = pattern.matcher(a);
-            Matcher matcherB = pattern.matcher(b);
+            String[] partsA = new String[2];
+            partsA[0] = a.split("\\d")[0];
+            partsA[1] = a.split("\\D+")[1];
 
-            List<String> partsA = new ArrayList<>();
-            while (matcherA.find()) {
-                partsA.add(matcherA.group());
-            }
+            String[] partsB = new String[2];
+            partsB[0] = b.split("\\d")[0];
+            partsB[1] = b.split("\\D+")[1];
 
-            List<String> partsB = new ArrayList<>();
-            while (matcherB.find()) {
-                partsB.add(matcherB.group());
-            }
-
-            if(!partsA.get(0).toLowerCase().equals(partsB.get(0).toLowerCase())) {
-                return partsA.get(0).toLowerCase().compareTo(partsB.get(0).toLowerCase());
-            } else if (Integer.parseInt(partsA.get(1)) != Integer.parseInt(partsB.get(1))) {
-                return Integer.parseInt(partsA.get(1)) - Integer.parseInt(partsB.get(1));
+            if(!partsA[0].toLowerCase().equals(partsB[0].toLowerCase())) {
+                return partsA[0].toLowerCase().compareTo(partsB[0].toLowerCase());
+            } else if (Integer.parseInt(partsA[1]) != Integer.parseInt(partsB[1])) {
+                return Integer.parseInt(partsA[1]) - Integer.parseInt(partsB[1]);
             } else {
                 return 0;
             }
